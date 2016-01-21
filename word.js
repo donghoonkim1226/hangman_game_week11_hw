297b9c1083fcb665f6e8ae685b3dea32638b6bdc
dong hoon kim <donghoonkim1226@gmail.com>
2016-01-21 12:58:12 -0500

    created word.js and put in the function Word

---
+++ word.js
@@ -0,0 +1,10 @@
+var Letter = require ("./letter.js");
+
+function Word(wrd){
+	this.word = wrd;
+	this.lets = [];
+	this.found = false;
+	this.getLets = function (){
+		for(i=0; i <= word.length; i++)
+	}
+}
