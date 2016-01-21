50d804420dfe9bddd4b50896fda9e0701bbeb4f2
dong hoon kim <donghoonkim1226@gmail.com>
2016-01-21 11:14:21 -0500

    Initial commit

---
+++ letter.js
@@ -0,0 +1,11 @@
+function Letter(let){
+	this.charac = "let";
+	this.appear = false;
+	this.letterRender = function(){
+		if (this.charac === false){
+			console.log("_");
+		} else {
+			console.log(this.charac);
+		}
+	}
+}
