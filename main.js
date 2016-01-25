fb42b8b7fe60a33f2d60ffe1c369ba582a957735
dong hoon kim <donghoonkim1226@gmail.com>
2016-01-25 15:14:11 -0500

    finished the last part to end the game.

--- main.js
+++ main.js
@@ -20,14 +20,26 @@ var game = {
 			var findHowManyOfUserGuess = currentWrd.checkIfLetterFound(result.guessLetter); // not fully understanding this part
 			if (findHowManyOfUserGuess === 0){
 				console.log("You guessed incorrectly~!");
-				self.guessesRemaining --;
+				self.guessesRemaining --; // not fully understanding this part
 			} else {
 					console.log("You guessed right~!");
 				if (startingWord.didWeFindTheWord() === true){ // not fully understanding this part
 					console.log("You Won~!");
 					return 1;
-				} 
+				}
+			}
+			console.log("Guesses reamining " + guessesRemaining);
+			console.log(startingWord.wordRender()); // not fully understanding this part
+			if (guessesRemaining > 0 && startingWord === false){
+				self.keepPromptingUser(); // not fully understanding this part
+			} else if (guessesRemaining === 0){
+					console.log("Game over bro~!");
+					console.log("The word you were guessing was " + self.startingWord.word);
+			} else {
+				console.log(startingWord.wordRender());
 			}
 		});
 	}
-}
+};
+
+game.startGame();
